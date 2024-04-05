169d18f0301932b0f82ff6bfd5f0b4d16ddfc942
Hexa <84851013+ctrl-hexa@users.noreply.github.com>
2024-04-05 11:55:24 +0200

    wip: navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../styles/navbar.module.css'
+export default function NavBar() {
+  return (
+    <div className={styles.navbar}>
+      <a>hello</a>
+      <a>lorem</a>
+      <a>ipsum</a>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './routes/App.jsx'
 import './styles/index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './routes/ErrorPage.jsx'
+import Account from './routes/Account.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'account',
+        element: <Account />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
