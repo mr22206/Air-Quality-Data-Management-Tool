c0caa6cb73bdc3dbccdf8ad2b111b4f7931ce8e6
Hexa <84851013+ctrl-hexa@users.noreply.github.com>
2024-04-15 18:22:55 +0200

    feat(data): ui improvements and select request

--- src/routes/Data.jsx
+++ src/routes/Data.jsx
@@ -1,38 +1,48 @@
-import { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 export default function Data() {
   const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/api/', {
+  const handleChange = async (event) => {
+    const selectedValue = event.target.value
+    setLoading(true) // Set loading to true when fetching data
+    setError(null) // Reset error state
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/${selectedValue}`,
+        {
           headers: { accept: 'application/json' },
-        })
-
-        if (!response.ok) {
-          throw new Error('Failed to fetch data')
         }
-        const result = await response.json()
-        setData(result)
-      } catch (error) {
-        setError(error.message)
-      } finally {
-        setLoading(false)
+      )
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch data')
       }
+
+      const result = await response.json()
+      setData(result)
+    } catch (error) {
+      setError(error.message)
+    } finally {
+      setLoading(false) // Set loading to false after data fetching completes
     }
-
-    fetchData()
-  }, [])
-
-  if (loading) {
-    return <div>Loading...</div>
   }
 
-  if (error) {
-    return <div>Error: {error}</div>
-  }
-  return <div>{data.message}</div>
+  return (
+    <div>
+      <select onChange={handleChange}>
+        <option value="">Select an option</option>
+        <option value="getSensor">Agency</option>
+        <option value="user/bordeaux">User</option>
+        {/* Add more options as needed */}
+      </select>
+
+      {loading && <div>Loading...</div>}
+      {error && <div>Error: {error}</div>}
+      {data && <div>{data.message}</div>}
+    </div>
+  )
 }
