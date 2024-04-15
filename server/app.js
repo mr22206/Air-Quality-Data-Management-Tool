c8926973522191c3005836b6dfe76acae9ba548e
Azuka <100121407+HunterFuzzo@users.noreply.github.com>
2024-04-15 14:52:38 +0200

    a

--- server/app.js
+++ server/app.js
@@ -3,57 +3,57 @@ import { getAgency, getUser, getSensor, getReport, getGasEmissions, getMostPollu
 
 const app = express()
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.send('Online!')
 })
 // Login page
 
-app.get("/agency", async (req, res) => {
+app.get("/api/agency", async (req, res) => {
     res.send(await getAgency());
 });
 
-app.get("/user/bordeaux", async (req, res) => {
+app.get("/api/user/bordeaux", async (req, res) => {
     res.send(await getUser());
 });
 
-app.get("/sensor", async (req, res) => {
+app.get("/api/sensor", async (req, res) => {
     res.send(await getSensor());
 });
 
-app.get("/report", async (req, res) => {
+app.get("/api/report", async (req, res) => {
     res.send(await getReport());
 });
 
-app.get("/gas-emissions", async (req, res) => {
+app.get("/api/gas-emissions", async (req, res) => {
     res.send(await getGasEmissions());
 });
 
-app.get("/most-polluting", async (req, res) => {
+app.get("/api/most-polluting", async (req, res) => {
     res.send(await getMostPolluting());
 });
 
-app.get("/sort-report", async (req, res) => {
+app.get("/api/sort-report", async (req, res) => {
     res.send(await sortReport());
 });
 
-app.get("/agent", async (req, res) => {
+app.get("/api/agent", async (req, res) => {
     res.send(await getAgent());
 });
 
-app.get("/emission-sum", async (req, res) => {
+app.get("/api/emission-sum", async (req, res) => {
     res.send(await getEmissionSum());
 });
 
-app.get("/prod-rate", async (req, res) => {
+app.get("/api/prod-rate", async (req, res) => {
     res.send(await getProdRate());
 });
 
-app.get("/report-list/:gasType", async (req, res) => {
+app.get("/api/report-list/:gasType", async (req, res) => {
     const gasType = req.params.gasType;
     res.send(await getReportList(gasType));
 });
 
-app.get("/region-list", async (req, res) => {
+app.get("/api/region-list", async (req, res) => {
     res.send(await getRegionList());
 });
 
