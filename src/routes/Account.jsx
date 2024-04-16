2f0112ba652eb04dc605c52a4a067f3ed17b9857
Hexa <84851013+ctrl-hexa@users.noreply.github.com>
2024-04-16 13:44:07 +0200

    feat(login): ui is now cleaner

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -21,44 +21,53 @@ export default function Account() {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col w-80 gap-3"
-    >
-      <div className="flex ">
-        <label className="bg-blue-400 rounded-l-md w-20 h-16 flex justify-center items-center">
-          <img className="w-8 h-8" src="/user-icon.png"></img>
-        </label>
-        <input
-          className="rounded-r-md w-full p-4 bg-white text-black border border-blue-400"
-          placeholder="Username"
-          required
-          type="text"
-          {...register('username')}
-        />
-
-        {errors.username && <span>{errors.username.message}</span>}
-      </div>
-
-      <div className="flex  rounded-md">
-        <label className=" bg-blue-400 rounded-l-md  w-20 h-16  flex justify-center items-center">
-          <img className="w-8 h-8" src="/lock-icon.png"></img>
-        </label>
-        <input
-          className="rounded-r-md  w-full p-4 bg-white  text-black border border-blue-400"
-          type="password"
-          {...register('password')}
-          placeholder="Password"
-        />
-        {errors.password && <span>{errors.password.message}</span>}
-      </div>
-
-      <button
-        className=" h-12 w-80 text-white bg-blue-400 rounded-md hover:bg-blue-500 "
-        type="submit"
+    <div className="bg-white rounded-xl w-[900px] flex justify-between items-center p-2">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col w-full p-6 "
       >
-        SIGN IN
-      </button>
-    </form>
+        <h1 className="text-black font-bold ">Welcome back !</h1>
+        <p className="text-gray-500 pb-10 pl-1">
+          Please enter credentials to login.
+        </p>
+        <div className="flex  flex-col gap-3">
+          <div className="flex ">
+            <label className="bg-blue-400 rounded-l-md w-20 h-16 flex justify-center items-center">
+              <img className="w-6 h-6 " src="/user-icon.png"></img>
+            </label>
+            <input
+              className="rounded-r-md w-full p-4 bg-white text-black border border-blue-400"
+              placeholder="Username"
+              required
+              type="text"
+              {...register('username')}
+            />
+
+            {errors.username && <span>{errors.username.message}</span>}
+          </div>
+
+          <div className="flex  rounded-md">
+            <label className=" bg-blue-400 rounded-l-md  w-20 h-16  flex justify-center items-center">
+              <img className="w-6 h-6" src="/lock-icon.png"></img>
+            </label>
+            <input
+              className="rounded-r-md  w-full p-4 bg-white  text-black border border-blue-400"
+              type="password"
+              {...register('password')}
+              placeholder="Password"
+            />
+            {errors.password && <span>{errors.password.message}</span>}
+          </div>
+
+          <button
+            className=" h-12 w-full text-white bg-blue-400 rounded-md hover:bg-blue-500 text-l"
+            type="submit"
+          >
+            SIGN IN
+          </button>
+        </div>
+      </form>
+      <img className="ml-32 w-[372px] rounded-r-xl" src="/login.png" />
+    </div>
   )
 }
--- src/routes/Data.jsx
+++ src/routes/Data.jsx
@@ -1,33 +1,42 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
 
 export default function Data() {
   const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(false)
+
   const [error, setError] = useState(null)
 
   const handleChange = async (event) => {
     const selectedValue = event.target.value
-    setLoading(true) // Set loading to true when fetching data
+
     setError(null) // Reset error state
 
     try {
-      const response = await fetch(
-        `http://localhost:3000/api/${selectedValue}`,
-        {
-          headers: { accept: 'application/json' },
-        }
-      )
+      // Show pending toast notification
+      const promise = fetch(`http://localhost:3000/api/${selectedValue}`, {
+        headers: { accept: 'application/json' },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Failed to fetch data')
+          }
+          return response.json()
+        })
+        .then((result) => {
+          setData(result)
+          // Show success toast notification
+        })
 
-      if (!response.ok) {
-        throw new Error('Failed to fetch data')
-      }
-
-      const result = await response.json()
-      setData(result)
+      // Show pending toast notification
+      toast.promise(promise, {
+        pending: 'Retrieving Data',
+        success: 'Data Retrieval successful 👌',
+        error: 'Data Retrieval Rejected 🤯',
+      })
     } catch (error) {
       setError(error.message)
-    } finally {
-      setLoading(false) // Set loading to false after data fetching completes
+      // Show error toast notification
+      toast.error('Login rejected 🤯')
     }
   }
 
@@ -36,11 +45,10 @@ export default function Data() {
       <select onChange={handleChange}>
         <option value="">Select an option</option>
         <option value="getSensor">Agency</option>
-        <option value="user/bordeaux">User</option>
+        <option value="">User</option>
         {/* Add more options as needed */}
       </select>
 
-      {loading && <div>Loading...</div>}
       {error && <div>Error: {error}</div>}
       {data && <div>{data.message}</div>}
     </div>
