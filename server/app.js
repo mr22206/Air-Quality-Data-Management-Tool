18629533da904bfc0bd121d82dc236b4b1565c25
Azuka <100121407+HunterFuzzo@users.noreply.github.com>
2024-04-16 07:13:43 +0200

    send -> json

--- server/app.js
+++ server/app.js
@@ -4,62 +4,62 @@ import { getAgency, getUser, getSensor, getReport, getGasEmissions, getMostPollu
 const app = express()
 
 app.get("/api", (req, res) => {
-    res.send('Online!')
+    res.json('Online!')
 })
 // Login page
 
 app.get("/api/agency", async (req, res) => {
-    res.send(await getAgency());
+    res.json(await getAgency());
 });
 
 app.get("/api/user/bordeaux", async (req, res) => {
-    res.send(await getUser());
+    res.json(await getUser());
 });
 
 app.get("/api/sensor", async (req, res) => {
-    res.send(await getSensor());
+    res.json(await getSensor());
 });
 
 app.get("/api/report", async (req, res) => {
-    res.send(await getReport());
+    res.json(await getReport());
 });
 
 app.get("/api/gas-emissions", async (req, res) => {
-    res.send(await getGasEmissions());
+    res.json(await getGasEmissions());
 });
 
 app.get("/api/most-polluting", async (req, res) => {
-    res.send(await getMostPolluting());
+    res.json(await getMostPolluting());
 });
 
 app.get("/api/sort-report", async (req, res) => {
-    res.send(await sortReport());
+    res.json(await sortReport());
 });
 
 app.get("/api/agent", async (req, res) => {
-    res.send(await getAgent());
+    res.json(await getAgent());
 });
 
 app.get("/api/emission-sum", async (req, res) => {
-    res.send(await getEmissionSum());
+    res.json(await getEmissionSum());
 });
 
 app.get("/api/prod-rate", async (req, res) => {
-    res.send(await getProdRate());
+    res.json(await getProdRate());
 });
 
 app.get("/api/report-list/:gasType", async (req, res) => {
     const gasType = req.params.gasType;
-    res.send(await getReportList(gasType));
+    res.json(await getReportList(gasType));
 });
 
 app.get("/api/region-list", async (req, res) => {
-    res.send(await getRegionList());
+    res.json(await getRegionList());
 });
 
 app.use((err, req, res, next) => {
     console.error(err)
-    res.status(500).send('Something went wrong!')
+    res.status(500).json('Something went wrong!')
 })
 
 app.listen(3000, () => {
