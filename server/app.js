7a705a9f179ea1f88dd6b716c02e257e749a995a
Azuka <100121407+HunterFuzzo@users.noreply.github.com>
2024-04-16 08:23:59 +0200

    a

--- server/app.js
+++ server/app.js
@@ -16,116 +16,64 @@ import {
 
 import cors from 'cors'
 const app = express()
+const corsOptions = {
+  origin: 'http://localhost:5173',
+}
 
-<<<<<<<<< Temporary merge branch 1
-app.get("/", (req, res) => {
-=========
-app.get("/api", (req, res) => {
->>>>>>>>> Temporary merge branch 2
-    res.send('Online!')
+app.use(cors(corsOptions))
+a
+app.get('/api', (req, res) => {
+  res.json({ message: 'Online!' })
 })
 // Login page
 
-<<<<<<<<< Temporary merge branch 1
-app.get("/agency", async (req, res) => {
-    res.send(await getAgency());
-});
+app.get('/api/agency', async (req, res) => {
+  res.send(await getAgency())
+})
 
-app.get("/user/bordeaux", async (req, res) => {
-    res.send(await getUser());
-});
+app.get('/api/user/bordeaux', async (req, res) => {
+  res.send(await getUser())
+})
 
-app.get("/sensor", async (req, res) => {
-    res.send(await getSensor());
-});
+app.get('/api/sensor', async (req, res) => {
+  res.json(await getSensor())
+})
 
-app.get("/report", async (req, res) => {
-    res.send(await getReport());
-});
+app.get('/api/report', async (req, res) => {
+  res.send(await getReport())
+})
 
-app.get("/gas-emissions", async (req, res) => {
-    res.send(await getGasEmissions());
-});
+app.get('/api/gas-emissions', async (req, res) => {
+  res.send(await getGasEmissions())
+})
 
-app.get("/most-polluting", async (req, res) => {
-    res.send(await getMostPolluting());
-});
+app.get('/api/most-polluting', async (req, res) => {
+  res.send(await getMostPolluting())
+})
 
-app.get("/sort-report", async (req, res) => {
-    res.send(await sortReport());
-});
+app.get('/api/sort-report', async (req, res) => {
+  res.send(await sortReport())
+})
 
-app.get("/agent", async (req, res) => {
-    res.send(await getAgent());
-});
+app.get('/api/agent', async (req, res) => {
+  res.send(await getAgent())
+})
 
-app.get("/emission-sum", async (req, res) => {
-    res.send(await getEmissionSum());
-});
+app.get('/api/emission-sum', async (req, res) => {
+  res.send(await getEmissionSum())
+})
 
-app.get("/prod-rate", async (req, res) => {
-    res.send(await getProdRate());
-});
+app.get('/api/prod-rate', async (req, res) => {
+  res.send(await getProdRate())
+})
 
-app.get("/report-list/:gasType", async (req, res) => {
-=========
-app.get("/api/agency", async (req, res) => {
-    res.send(await getAgency());
-});
+app.get('/api/report-list/:gasType', async (req, res) => {
+  const gasType = req.params.gasType
+  res.send(await getReportList(gasType))
+})
 
-app.get("/api/user/bordeaux", async (req, res) => {
-    res.send(await getUser());
-});
-
-app.get("/api/sensor", async (req, res) => {
-    res.send(await getSensor());
-});
-
-app.get("/api/report", async (req, res) => {
-    res.send(await getReport());
-});
-
-app.get("/api/gas-emissions", async (req, res) => {
-    res.send(await getGasEmissions());
-});
-
-app.get("/api/most-polluting", async (req, res) => {
-    res.send(await getMostPolluting());
-});
-
-app.get("/api/sort-report", async (req, res) => {
-    res.send(await sortReport());
-});
-
-app.get("/api/agent", async (req, res) => {
-    res.send(await getAgent());
-});
-
-app.get("/api/emission-sum", async (req, res) => {
-    res.send(await getEmissionSum());
-});
-
-app.get("/api/prod-rate", async (req, res) => {
-    res.send(await getProdRate());
-});
-
-app.get("/api/report-list/:gasType", async (req, res) => {
->>>>>>>>> Temporary merge branch 2
-    const gasType = req.params.gasType;
-    res.send(await getReportList(gasType));
-});
-
-<<<<<<<<< Temporary merge branch 1
-app.get("/region-list", async (req, res) => {
-=========
-app.get("/api/region-list", async (req, res) => {
->>>>>>>>> Temporary merge branch 2
-    res.send(await getRegionList());
-});
-
-app.use((err, req, res, next) => {
-    console.error(err)
-    res.status(500).send('Something went wrong!')
+app.get('/api/region-list', async (req, res) => {
+  res.send(await getRegionList())
 })
 
 app.listen(3000, () => {
