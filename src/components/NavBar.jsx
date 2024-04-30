acf2648cf6882ad7bba84ac88519665bc46c42bd
Hexa <84851013+ctrl-hexa@users.noreply.github.com>
2024-04-30 18:31:23 +0200

    chore(nav): css to tailwindcss

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,3 @@
-import styles from '../styles/navbar.module.css'
 //import { useLocation } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import { useLocation } from 'react-router-dom'
@@ -14,31 +13,47 @@ export default function NavBar() {
   const location = useLocation()
 
   return (
-    <div className={styles.navbar}>
+    <div className="bg-white w-[100%] h-[65px] fixed top-0 left-0 flex justify-center align-center z-50">
       <div className="flex justify-between items-center w-[1152px]">
-        <a href="/">
-          <img src="/giec.png" />
+        <a href="/" className="flex justify-center align-center ml-[32px]">
+          <img src="/giec.png" className="h-[60px] w-auto" />
         </a>
-        <div className={styles.buttons}>
-          <a className={styles.index} href="/data">
-            <p>Data</p>
+        <div className="flex align-center h-[40px]">
+          <a
+            className="block text-center py-[14px] px-[20px] h-[100%] relative hover:text-hsl-110-53-51 hover:underline"
+            href="/data"
+          >
+            <p className="font font-semibold">Data</p>
           </a>
-          <a className={styles.index} href="/query">
+          <a
+            className="block text-center py-[14px] px-[20px] h-[100%] relative hover:text-hsl-110-53-51 hover:underline"
+            href="/query"
+          >
             <p>Query</p>
           </a>
           <a
-            className={isLoggedIn ? styles.index : styles.disabled}
+            className={
+              isLoggedIn
+                ? 'block text-center py-[14px] px-[20px] h-[100%] relative hover:text-hsl-110-53-51 hover:underline'
+                : 'block text-center py-[14px] px-[20px] h-[100%] relative text-gray-600'
+            }
             href={askAiLink}
           >
             <p>Ask ai</p>
           </a>
           {!isLoggedIn ? (
-            <a className={styles.login} href="/account">
-              <p>Login</p>
+            <a
+              className="block text-center h-[40px] w-[128px] relative bg-[#008037] text-white rounded-[4px] mr-[64px] hover:text-hsl-110-53-51 hover:underline"
+              href="/account"
+            >
+              <p className="text-white">Login</p>
             </a>
           ) : (
-            <button className={styles.login} onClick={() => logout()}>
-              <p>Logout</p>
+            <button
+              className="block text-center h-[40px] w-[128px] relative bg-[#008037] text-white rounded-[4px] mr-[64px] hover:text-hsl-110-53-51 hover:underline"
+              onClick={() => logout()}
+            >
+              <p className="text-white">Logout</p>
             </button>
           )}
         </div>
