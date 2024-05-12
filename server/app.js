6fce273ef8f3cbdd24b83c67eee6ee11d071ca14
mohammad <mohammad.rezki@viacesi.fr>
2024-05-12 23:20:28 +0200

    error heroku page acceuil bug

--- server/app.js
+++ server/app.js
@@ -235,7 +235,7 @@ app.post('/api/request', authenticateToken, async (req, res) => {
   }
 })
 
-app.listen(process.env.VITE_PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on http://localhost:...`);
 })
 
