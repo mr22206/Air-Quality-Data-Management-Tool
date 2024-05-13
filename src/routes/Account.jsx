eec929dd6db8f8eecccd0bec75bc4d24c3c8166e
mohammad <mohammad.rezki@viacesi.fr>
2024-05-13 21:57:35 +0200

    error heroku route ajout fichier dis kikit

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -23,23 +23,22 @@ export default function Account() {
     setIsLoggingIn(true)
 
     axios
-    .post(process.env.JAWSDB_MARIA_URL + '/api/creds', { username: username, password: password })
+    .post(`${import.meta.env.VITE_API_URL}/api/creds`, { username: username, password: password })
     .then((response) => {
-      setIsLoggingIn(false);
-      if (response.data.token) {
-        login(response.data.token);
-        toast('Login successful 👌', { type: 'success' });
-        navigate('/ask-ai');
-      } else {
-        // Show error toast for invalid credentials
-        toast('Invalid credentials 🤯', { type: 'error' });
-      }
-    })
-    .catch((error) => {
-      setIsLoggingIn(false);
-      toast.error('An error occurred 🤯');
-    })
-  
+        setIsLoggingIn(false)
+        if (response.data.token) {
+          login(response.data.token)
+          toast('Login successful 👌', { type: 'success' })
+          navigate('/ask-ai')
+        } else {
+          // Show error toast for invalid credentials
+          toast('Invalid credentials 🤯', { type: 'error' })
+        }
+      })
+      .catch((error) => {
+        setIsLoggingIn(false)
+        toast.error('An error occurred 🤯')
+      })
   }
   //TODO: get navbar here and use shadows to make it pop
   return (
