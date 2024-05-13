6aeef534f47aa512352faa50d4212123eebdc118
mohammad <mohammad.rezki@viacesi.fr>
2024-05-13 17:45:23 +0200

    mosdqfhh

--- server/app.js
+++ server/app.js
@@ -14,6 +14,13 @@ app.use(express.json())
 app.use(cors(corsOptions))
 app.use(express.static('dist'));
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 
 app.get('/api', (req, res) => {
