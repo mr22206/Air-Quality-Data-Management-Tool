087a1c4e217e394aea5f70657e2e130f0f517bc8
mohammad <mohammad.rezki@viacesi.fr>
2024-05-13 22:34:13 +0200

    error heroku route ajout fichier dis kikit

--- server/utils/queries.js
+++ server/utils/queries.js
@@ -1,13 +1,23 @@
 import mysql from 'mysql2'
 
-export const pool = mysql
-  .createPool({
+if (process.env.NODE_ENV === 'production') {
+    const pool =  mysql.createPool({
+    host: 'eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306',
+    user: 'alahupbjep6bxyud',
+    password: 'll4l3xwkurxlz7zo',
+    database: 'd4fcztkaganmi82l'
+
+  })
+  .promise()
+} else {
+const pool =  mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'datax',
   })
   .promise()
+}
 
 // Fonctions pour récupérer les données de la base de données
 
