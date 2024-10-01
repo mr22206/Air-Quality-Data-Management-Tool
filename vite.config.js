b9d931d263452736b919d84a2ece668cf9a96e79
mohammad <mohammad.rezki@viacesi.fr>
2024-10-01 10:55:41 +0200

    Data-x-giec

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // Remplace 'NOM_DE_TON_REPO' par le nom de ton dépôt
 export default defineConfig({
   plugins: [react()],
-  base: '/Data-x-Giec/site-giec/',  // Remplace par le chemin de ton repo
+  base: '/Air-Quality-Data-Management-Tool/',  // Remplace par le chemin de ton repo
 });
